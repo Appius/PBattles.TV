f862febbe5fd5168c0c6bb0d6ed696617f118345
Vladyslav_Ushakov <v.ushakov@kpi.in.ua>
2014-04-30 12:36:57 +0300

    Add new folder for libs. Add angular to index page and test it.

---
+++ frontend/app/js/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var index = angular.module('index', [/*
+    'index.filters',
+    'index.services',
+    'index.directives'*/
+]);
+
+/*
+angular.module('myApp', [
+    'ngRoute',
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives'
+]);*/
+
+var room = angular.module('room', [/*
+ 'index.filters',
+ 'index.services',
+ 'index.directives'*/
+]);
---
+++ frontend/app/js/controllers/indexCtrl.js
@@ -0,0 +1,7 @@
+'use strict';
+
+index.controller('indexCtrl',
+    function indexCtrl($scope) {
+        $scope.version = '1.2.2.2';
+    }
+);
